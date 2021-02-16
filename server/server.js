9753f5f5484365b259fa0658d4e146a6e35606d7
Riley Seaburg <riley@rileyseaburg.com>
2021-02-15 21:15:13 -0500

    Add server & DB models

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import config from './../config/config.mjs'
+import mongoose from 'mongoose';
+import app from './express.js'
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri,{useNewUrlParser: true,
+
+useCreateIndex: true, useUnifiedTopology: true,})
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${config.mongoUri}`);
+})
+
+console.log(config.mongoUri)
+
+ app.listen(config.port, (err) => {
+  if (err) {
+    console.log(err)
+  }
+  console.info('Server started on port %s.', config.port)
+})
