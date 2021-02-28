531fec8048ce31624209028413cdd8f4715932b8
Riley Seaburg <riley@rileyseaburg.com>
2021-02-28 13:21:08 -0500

    CU-nuzqd7 [Accepted] - Add User API Endpoints @RileySeaburg

--- server/express.js
+++ server/express.js
@@ -5,6 +5,7 @@ import compress from 'compression';
 import cors from 'cors'
 import helmet from 'helmet'
 import Template from '../template.mjs';
+import userRoutes from './routes/user.routes'
 const app = express();
 // configure express
 
@@ -14,7 +15,7 @@ app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
-
+app.use('/', userRoutes);
 app.get('/', (req, res) => {
     res.status(200).send(Template())
 })
---
+++ server/routes/user.routes.js
@@ -0,0 +1,32 @@
+/*
+/api/users for the following:
+-Listing users with GET 
+-Creating a new user with POST 
+/api/users/:userId for the following:  
+-Fetching a user with GET 
+-Updating a user with PUT 
+-Deleting a user with DELETE
+*/
+import express from 'express'
+import userCtrl from '../controllers/user.controller'
+
+const router = express.Router()
+
+router.route('/api/users')
+    .get(userCtrl.list)
+    .post(userCtrl.create)
+
+router.route('/api/users/:userId')
+    .get(userCtrl.read)
+    .put(userCtrl.update)
+    .delete(userCtrl.remove)
+
+router.param('userId', userCtrl.userByID)
+router.param('userId', userCtrl.userById)
+    
+router.route('/api/users').post(userCtrl.create)
+router.route('/api/users').get(userCtrl.list)
+router.route('/api/users/:userId').get(userCtrl.read)
+router.route('/api/users/:userId').put(userCtrl.update)
+router.route('/api/users/:userId').delete(userCtrl.remove)
+export default router
